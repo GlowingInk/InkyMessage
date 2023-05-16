a12759d36b1f6b327342b1f797f3c8037a84ef7b
Daniil Z <idanix@list.ru>
2023-05-16 19:47:14 +0300

    Add optional repeat modifier

---
+++ src/main/java/ink/glowing/text/style/modifier/extra/RepeatModifier.java
@@ -0,0 +1,42 @@
+package ink.glowing.text.style.modifier.extra;
+
+import ink.glowing.text.InkyMessage;
+import ink.glowing.text.style.modifier.StyleModifier;
+import net.kyori.adventure.text.Component;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Unmodifiable;
+
+import java.util.List;
+
+public class RepeatModifier implements StyleModifier.Plain {
+    private static final RepeatModifier INSTANCE = new RepeatModifier();
+    public static @NotNull RepeatModifier repeatModifier() {
+        return INSTANCE;
+    }
+
+    @Override
+    public @NotNull Component modify(@NotNull Component text, @NotNull String param, @NotNull String value) {
+        int count;
+        try {
+            count = Integer.parseInt(param);
+        } catch (NumberFormatException exception) {
+            return text;
+        }
+        if (count == 0) return text;
+        var builder = Component.text().append(text);
+        for (int i = 0; i < count; i++) {
+            builder.append(text);
+        }
+        return builder.build();
+    }
+
+    @Override
+    public @NotNull @Unmodifiable List<String> read(@NotNull InkyMessage.Resolver resolver, @NotNull Component text) {
+        return List.of();
+    }
+
+    @Override
+    public @NotNull String name() {
+        return "repeat";
+    }
+}
