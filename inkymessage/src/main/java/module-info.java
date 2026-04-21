4859e4562a32ac07974689c54e95d6a2a619860a
Daniil Z. <idanix@list.ru>
2026-04-21 23:57:56 +0400

    Extend module usage

--- inkymessage/src/main/java/module-info.java
+++ inkymessage/src/main/java/module-info.java
@@ -1,5 +1,16 @@
 module inkymessage {
     requires net.kyori.adventure.key;
-    requires org.jetbrains.annotations;
     requires net.kyori.adventure.api;
+    requires static org.jetbrains.annotations;
+
+    exports ink.glowing.text;
+    exports ink.glowing.text.modifier;
+    exports ink.glowing.text.modifier.standard;
+    exports ink.glowing.text.placeholder;
+    exports ink.glowing.text.processor;
+    exports ink.glowing.text.replace;
+    exports ink.glowing.text.symbolic;
+    exports ink.glowing.text.symbolic.standard;
+    exports ink.glowing.text.utils;
+    exports ink.glowing.text.utils.function;
 }
